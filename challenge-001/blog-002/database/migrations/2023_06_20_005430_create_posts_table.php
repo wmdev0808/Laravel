7fa9babaef8c5e78391c177f815d2a4158ba9752
Paul <wmdev0808@gmail.com>
2023-06-28 01:52:52 -0700

    -- Update migrations for posts and comments creation

--- challenge-001/blog-002/database/migrations/2023_06_20_005430_create_posts_table.php
+++ challenge-001/blog-002/database/migrations/2023_06_20_005430_create_posts_table.php
@@ -12,7 +12,7 @@ return new class extends Migration {
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->foreignId('category_id');
             $table->string('slug')->unique();
             $table->string('title');
--- challenge-001/blog-002/database/migrations/2023_06_28_084503_create_comments_table.php
+++ challenge-001/blog-002/database/migrations/2023_06_28_084503_create_comments_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,9 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('post_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->text('body');
             $table->timestamps();
         });
     }
