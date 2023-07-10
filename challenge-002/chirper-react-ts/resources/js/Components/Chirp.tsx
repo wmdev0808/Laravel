886f09d645cb6bb71324711cc95030ad4b066faf
Paul <wmdev0808@gmail.com>
2023-07-10 03:08:43 -0700

    -- Delete Chirps

--- challenge-002/chirper-react-ts/resources/js/Components/Chirp.tsx
+++ challenge-002/chirper-react-ts/resources/js/Components/Chirp.tsx
@@ -82,6 +82,13 @@ export default function Chirp({ chirp }: ChirpProps) {
                                 >
                                     Edit
                                 </button>
+                                <Dropdown.Link
+                                    as="button"
+                                    href={route("chirps.destroy", chirp.id)}
+                                    method="delete"
+                                >
+                                    Delete
+                                </Dropdown.Link>
                             </Dropdown.Content>
                         </Dropdown>
                     )}
