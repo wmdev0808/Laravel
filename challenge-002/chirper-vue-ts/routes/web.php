47d224884be9155a9df9bb5555f4396da66b50b6
Paul <wmdev0808@gmail.com>
2023-07-14 07:56:24 -0700

    -- Delete Chirps

--- challenge-002/chirper-vue-ts/routes/web.php
+++ challenge-002/chirper-vue-ts/routes/web.php
@@ -31,7 +31,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::resource('chirps', ChirpController::class)
-    ->only(['index', 'store', 'update'])
+    ->only(['index', 'store', 'update', 'destroy'])
     ->middleware(['auth', 'verified']);
 
 Route::middleware('auth')->group(function () {
