dab155c95415c14546819e1acb38e25b16519087
Paul <wmdev0808@gmail.com>
2023-07-14 08:04:55 -0700

    -- Send notifications when a new chirp is created

--- challenge-002/chirper-vue-ts/app/Models/Chirp.php
+++ challenge-002/chirper-vue-ts/app/Models/Chirp.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Events\ChirpCreated;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -12,6 +13,10 @@ class Chirp extends Model
 
     protected $fillable = [
         'message',
+    ];
+
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
     ];
 
     public function user(): BelongsTo
