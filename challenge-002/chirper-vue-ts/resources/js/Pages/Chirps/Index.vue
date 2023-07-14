5b890b152c4b3496a0ec904e1195e20db106e13e
Paul <wmdev0808@gmail.com>
2023-07-14 07:19:05 -0700

    -- Create Chirps

---
+++ challenge-002/chirper-vue-ts/resources/js/Pages/Chirps/Index.vue
@@ -0,0 +1,34 @@
+<script setup>
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import InputError from "@/Components/InputError.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import { useForm, Head } from "@inertiajs/vue3";
+
+const form = useForm({
+    message: "",
+});
+</script>
+
+<template>
+    <Head title="Chirps" />
+
+    <AuthenticatedLayout>
+        <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+            <form
+                @submit.prevent="
+                    form.post(route('chirps.store'), {
+                        onSuccess: () => form.reset(),
+                    })
+                "
+            >
+                <textarea
+                    v-model="form.message"
+                    placeholder="What's on your mind?"
+                    class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+                ></textarea>
+                <InputError :message="form.errors.message" class="mt-2" />
+                <PrimaryButton class="mt-4">Chirp</PrimaryButton>
+            </form>
+        </div>
+    </AuthenticatedLayout>
+</template>
--- challenge-002/chirper-vue-ts/routes/web.php
+++ challenge-002/chirper-vue-ts/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -28,6 +29,12 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+
+Route::resource('chirps', ChirpController::class)
+    ->only(['index', 'store'])
+    ->middleware(['auth', 'verified']);
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
