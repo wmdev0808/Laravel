57bf5c8a3c1948e1112705c70dde4faf98f3ff97
Paul <wmdev0808@gmail.com>
2023-07-14 06:24:34 -0700

    -- Show Chirps

--- challenge-002/chirper-react-ts/app/Http/Controllers/ChirpController.php
+++ challenge-002/chirper-react-ts/app/Http/Controllers/ChirpController.php
@@ -16,7 +16,7 @@ class ChirpController extends Controller
     public function index(): Response
     {
         return Inertia::render('Chirps/Index', [
-
+            'chirps' => Chirp::with('user:id,name')->latest()->get(),
         ]);
     }
 
