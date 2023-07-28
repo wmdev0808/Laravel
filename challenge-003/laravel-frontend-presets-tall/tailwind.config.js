9bcf7860363f6d1a2c04f46b5340208eecbd2a09
Paul <wmdev0808@gmail.com>
2023-07-28 17:13:13 +0800

    -- Update tailwind config based on ESM syntax

--- challenge-003/laravel-frontend-presets-tall/tailwind.config.js
+++ challenge-003/laravel-frontend-presets-tall/tailwind.config.js
@@ -1,31 +1,30 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
+import typography from "@tailwindcss/typography";
 
-module.exports = {
+export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+                sans: ["Inter var", ...defaultTheme.fontFamily.sans],
             },
         },
     },
     variants: {
         extend: {
-            backgroundColor: ['active'],
-        }
+            backgroundColor: ["active"],
+        },
     },
     content: [
-        './app/**/*.php',
-        './resources/**/*.html',
-        './resources/**/*.js',
-        './resources/**/*.jsx',
-        './resources/**/*.ts',
-        './resources/**/*.tsx',
-        './resources/**/*.php',
-        './resources/**/*.vue',
-        './resources/**/*.twig',
+        "./app/**/*.php",
+        "./resources/**/*.html",
+        "./resources/**/*.js",
+        "./resources/**/*.jsx",
+        "./resources/**/*.ts",
+        "./resources/**/*.tsx",
+        "./resources/**/*.php",
+        "./resources/**/*.vue",
+        "./resources/**/*.twig",
     ],
-    plugins: [
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/typography'),
-    ],
-}
+    plugins: [forms, typography],
+};
