97c817b6a1037e5e550990fa1fe24c8e0886bd1f
Paul <wmdev0808@gmail.com>
2023-08-10 22:18:29 +0800

    -- Preserve scroll position using preserve-scroll prop of <Link> component

--- challenge-004/001-inertia-basic/resources/js/Pages/Users.vue
+++ challenge-004/001-inertia-basic/resources/js/Pages/Users.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
+import { Link } from "@inertiajs/vue3";
 import Nav from "../Shared/Nav.vue";
+
+defineProps<{ time: string }>();
 </script>
 
 <template>
     <h1 class="text-4xl font-bold">Users</h1>
 
     <Nav />
+
+    <div style="margin-top: 400px">
+        <p>The current time is {{ time }}.</p>
+
+        <Link href="/users" class="text-blue-500" preserve-scroll>
+            Refresh
+        </Link>
+    </div>
 </template>
--- challenge-004/001-inertia-basic/routes/web.php
+++ challenge-004/001-inertia-basic/routes/web.php
@@ -28,9 +28,11 @@ Route::get('/', function () {
 });
 
 Route::get('/users', function () {
-    sleep(2);
+    // sleep(2);
 
-    return Inertia::render('Users');
+    return Inertia::render('Users', [
+        'time' => now()->toTimeString()
+    ]);
 });
 
 Route::get('/settings', function () {
